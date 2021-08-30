1da5003b8a83122f6d3d5b36df25fa579a157548
Gino Swanepoel <ginoleemusic@live.com>
2021-08-30 02:51:45 +0200

    added popular component

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { COLORS } from "../styles/colors";
 
 const HomeScreen = () => {
   return (
     <View style={styles.appContainer}>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.appTitle}>hero</Text>
-          <Text style={{ ...styles.p, fontSize: 7, marginTop: -8, left: 3 }}>
-            the Superhero Encyclopedia
-          </Text>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.appTitle}>hero</Text>
+            <Text style={{ ...styles.p, fontSize: 7, marginTop: -8, left: 3 }}>
+              the Superhero Encyclopedia
+            </Text>
+          </View>
         </View>
-      </View>
+        <View style={styles.popularContainer}>
+          <Text style={styles.h4}>Popular</Text>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -20,22 +25,15 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    justifyContent: "space-around",
-    alignItems: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
     backgroundColor: COLORS.beige,
-    padding: 30,
-    paddingTop: 40,
+    padding: 20,
+    // paddingTop: 40,
   },
-  header: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: -10,
-  },
-  appTitle: {
-    fontFamily: "Righteous_400Regular",
-    fontSize: 50,
+  h4: {
+    fontFamily: "Nunito_900Black",
+    fontSize: 20,
     textAlign: "left",
     color: COLORS.black,
   },
@@ -44,6 +42,23 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: "left",
     color: COLORS.black,
+  },
+  header: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  appTitle: {
+    fontFamily: "Righteous_400Regular",
+    fontSize: 50,
+    textAlign: "left",
+    color: COLORS.black,
+  },
+  popularContainer: {
+    justifyContent: "space-around",
+    alignItems: "flex-start",
   },
 });
 
