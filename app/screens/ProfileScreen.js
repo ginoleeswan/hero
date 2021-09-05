e3172686dfee233849f6fb4f78a900385e8cabcf
Gino Swanepoel <ginoleemusic@live.com>
2021-09-05 13:15:11 +0200

    added attributation

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { COLORS } from "../styles/colors";
 
 const ProfileScreen = () => {
   return (
     <View style={styles.appContainer}>
-      <View style={styles.header}>
-        <Text style={styles.appTitle}>profile</Text>
-      </View>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <Text style={styles.appTitle}>profile</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.p}>Designed by Gino Lee Swanepoel</Text>
+          <Text style={styles.p}>in React Native</Text>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -15,24 +21,33 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    justifyContent: "space-around",
-    alignItems: "center",
+    // justifyContent: "space-around",
+    // alignItems: "center",
     backgroundColor: COLORS.beige,
-    padding: 35,
-    paddingTop: 40,
+  },
+  p: {
+    fontFamily: "Flame-Regular",
+    fontSize: 15,
+    textAlign: "left",
+    color: COLORS.black,
   },
   header: {
     flexDirection: "row",
     width: "100%",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: -10,
+    paddingHorizontal: 15,
   },
   appTitle: {
     fontFamily: "Righteous_400Regular",
     fontSize: 50,
     textAlign: "left",
     color: COLORS.black,
+  },
+  footer: {
+    position: "absolute",
+    bottom: -600,
+    left: 15,
   },
 });
 
