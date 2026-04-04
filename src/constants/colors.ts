76ec9eaee835e3e07c4438d8e5626f0a787a3912
Gino Swanepoel <ginoleemusic@live.com>
2026-04-04 15:46:09 +0200

    feat: add colors and heroImages constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  beige: '#f5ebdc',
+  orange: '#E77333',
+  navy: '#293C43',
+  grey: '#A2A19B',
+  red: '#B5302B',
+  yellow: '#F9B222',
+  green: '#63A936',
+  skin: '#F7D173',
+  blue: '#15A1AB',
+  black: '#2D2D2D',
+  brown: '#502314',
+} as const;
