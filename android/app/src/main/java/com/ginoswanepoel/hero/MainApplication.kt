174d73a5360440258e0e3509cb6e619624c17477
Gino Swanepoel <67059115+ginoleeswan@users.noreply.github.com>
2026-04-09 07:58:11 +0200

    Fix project settings and update dependencies
    - Updated PRODUCT_NAME and TARGETED_DEVICE_FAMILY in project.pbxproj to use quotes for consistency.
    - Added missing newline at the end of Contents.json files for AppIcon and SplashScreenBackground.
    - Cleaned up Info.plist formatting and ensured proper structure.
    - Updated package.json dependencies to latest versions, including expo and related libraries.

---
+++ android/app/src/main/java/com/ginoswanepoel/hero/MainApplication.kt
@@ -0,0 +1,49 @@
+package com.ginoswanepoel.hero
+import com.facebook.react.common.assets.ReactFontManager
+
+import android.app.Application
+import android.content.res.Configuration
+
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactHost
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ExpoReactHostFactory
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactHost: ReactHost by lazy {
+    ExpoReactHostFactory.getDefaultReactHost(
+      context = applicationContext,
+      packageList =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        }
+    )
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    // @generated begin xml-fonts-init - expo prebuild (DO NOT MODIFY) sync-da39a3ee5e6b4b0d3255bfef95601890afd80709
+
+    // @generated end xml-fonts-init
+    DefaultNewArchitectureEntryPoint.releaseLevel = try {
+      ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
+    } catch (e: IllegalArgumentException) {
+      ReleaseLevel.STABLE
+    }
+    loadReactNative(this)
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
+}
